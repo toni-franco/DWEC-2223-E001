999aad9de3e33b05b821c16653e721c4017f10c0
toni-franco <toni.franco.pastor@gmail.com>
2022-09-23 12:39:28 +0200

    Exercicis UD2 i inici UD3

---
+++ dwc/UD3/codi-exemple/document-codi-exemple/document-01.js
@@ -0,0 +1,6 @@
+console.log(`location: ${document.location}`);
+console.log(`títol: ${document.title}`);
+console.log(`lastModified: ${document.lastModified}`);
+console.log(document.links[0].href);
+console.log(document.links.item(0).href);
+console.log(document.images[0].src);
