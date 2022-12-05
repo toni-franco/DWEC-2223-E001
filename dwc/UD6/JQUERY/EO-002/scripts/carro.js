11a83d6538f6764797a0142a49d88e4298916881
toni-franco <toni.franco.pastor@gmail.com>
2022-12-05 16:34:10 +0100

    Solucio EO-002

---
+++ dwc/UD6/JQUERY/EO-002/scripts/carro.js
@@ -0,0 +1,81 @@
+﻿function actualizaStockProducto($item, incremento)
+{
+	var actualizado = false;
+	var stock = parseInt($item.children(".stock").html().replace("Stock ", ""));
+	
+	if (stock+incremento >= 0)
+	{
+		stock += incremento;
+		$item.children(".stock").html("Stock " + stock);
+		if (stock == 0)
+			$item.find(".stock").addClass("agotado");
+		else
+			$item.find(".stock").removeClass("agotado");
+			
+		actualizado = true;
+	}
+	
+	return actualizado;
+}
+
+function actualizaNumeroProductosPedidos(incremento)
+{
+	var numProductosPedido = parseInt($("#citem").val());
+	numProductosPedido += incremento;
+	$("#citem").val(numProductosPedido);
+}
+
+function actualizaPrecioTotal($item, incremento)
+{
+	var precioPedido = parseInt($("#cprice").val());
+	precioPedido += parseInt(incremento);
+	$("#cprice").val(precioPedido + " €");
+}
+
+function eliminaProductoDelCarrito($item)
+{
+	var id = $item.attr("id");
+	id = id.substring(1);
+	
+	actualizaStockProducto($("#"+id), 1);
+	
+	actualizaNumeroProductosPedidos(-1);
+	
+	actualizaPrecioTotal($item, -parseInt($item.children(".price").html()));
+	
+	$item.remove();
+}
+
+function anyadeProductoAlCarrito($item)
+{
+	var $delete = $('<a href="" class="delete"></a>');
+
+	$delete.click(function (evento)
+	{
+		eliminaProductoDelCarrito($(this).parent());
+		return false;
+	});
+	
+	var id = "c"+$item.attr("id");  // id es de la forma ci1, ci2, .. (es decir, copia de i1, de i2...)
+        // Ahora anade a la copia un link con la clase delete y un evento cuando hacen click
+	$copia = $item.clone().attr("id", id).addClass('icart').prepend($delete); 
+	// Esconde de la copia el stock
+	$copia.children(":not(a)").addBack().css("cursor", "default").find(".stock").hide();
+	//añade al div del carro la copia. (<div id="cart_items" class="back"></div>)
+	$("#cart_items").prepend($copia);
+}
+
+$(function() 
+{
+	$(".item").dblclick(function()
+	{
+		if (actualizaStockProducto($(this), -1) == true)
+		{
+			actualizaNumeroProductosPedidos(1);
+			
+			actualizaPrecioTotal($(this), parseInt($(this).children(".price").html()));
+			
+			anyadeProductoAlCarrito($(this));
+		}
+	});
+});
---
+++ dwc/UD6/JQUERY/eventos/0009-eventos-on-2.htm
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>on demo</title>
+  <style>
+  p {
+    background: yellow;
+    font-weight: bold;
+    cursor: pointer;
+    padding: 5px;
+  }
+  p.over {
+    background: #ccc;
+  }
+  span {
+    color: red;
+  }
+  </style>
+  <script src="https://code.jquery.com/jquery-1.10.2.js"></script>
+</head>
+<body>
+ 
+<p>Click me!</p>
+<span></span>
+ 
+<script>
+var count = 0;
+$( "body" ).on( "click", "p", function() {
+  $( this ).after( "<p>Another paragraph! " + (++count) + "</p>" );
+});
+</script>
+ 
+</body>
+</html>
